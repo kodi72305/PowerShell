2aeaf7595ff22f35c7050d4a6426ab22fb92f1ea
izz1k <kiselevsa@szfk.ru>
2021-12-21 11:27:31 +0300

    PS

---
+++ PowerShell/Migration/Universal.ps1
@@ -0,0 +1,4 @@
+# get-aduser kiselevsa -Server srv3
+# get-aduser kiselevsa -Server szfk-vm-addc1
+# Get-Mailbox -Identity kiselevsa -DomainController srv3.szfk.biz
+Get-RemoteMailbox -DomainController szfk-vm-addc1.szfk.acron.ru
