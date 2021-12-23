a696a235e2ade8a8380a3d9be60550d5eb814c77
izz1k <kiselevsa@szfk.ru>
2021-12-23 16:46:30 +0300

    23

--- SrvMail.ps1
+++ SrvMail.ps1
@@ -1,20 +1,6 @@
-clear
-echo "biz"
-$User = "kiselevsa@szfk.biz"
-$PWord = ConvertTo-SecureString -String "UOqnmnq0" -AsPlainText -Force
-$UserCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://srvmail.szfk.biz/PowerShell/ -Authentication Kerberos -Credential $UserCredential
-Import-PSSession $Session -DisableNameChecking -AllowClobber
 
-Remove-PSSession $Session
 
 
 
 
 echo "acron"
-$User = "gokszfk\kiselevadm"
-$PWord = ConvertTo-SecureString -String "UOqnmnq9" -AsPlainText -Force
-$UserCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://szfk-vm-extsmb1/PowerShell/ -Authentication Kerberos -Credential $UserCredential
-Import-PSSession $Session -DisableNameChecking -AllowClobber
-
